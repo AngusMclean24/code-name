48ce16eefe4421d3cd99f732fee50b012e705303
Angus Mclean <48145878+AngusMclean24@users.noreply.github.com>
2021-11-20 20:15:44 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
+// Regular Websockets
 
-const serverAddress = 'https://simple-websocket-codename.glitch.me/';
-const socket = io('serverAddress')
+// const socket = new WebSocket('ws://localhost:8080');
 
-socket.on('message', text => {
-    /*
-    const el = document.createElement('li');
-    el.innerHTML = text;
-    document.querySelector('ul').appendChild(el)
-    */
+// // Listen for messages
+// socket.onmessage = ({ data }) => {
+//     console.log('Message from server ', data);
+// };
 
-
-    for (i=0; i<text.length; i++){
-        if (text[i] == 0) {
-            document.getElementById(i+1).style.backgroundColor = "rgba(95, 91, 91, 0.8)";
-            //document.getElementById(i+1).textContent = 20
-
-        }
-        
-        else{
-            document.getElementById(i+1).style.backgroundColor = "rgba(192, 67, 67, 0.8)";
-            //document.getElementById(i+1).textContent = 10
-        }
-    } 
-});
-
-/*
-document.querySelector('button').onclick = () => {
-
-    const text = document.querySelector('input').value;
-    socket.emit('message', text)
-    
-}
-*/
-function colourChange(item){
-    socket.emit('message', item)
-
-}
+// document.querySelector('button').onclick = () => {
+//     socket.send('hello');
+// }
