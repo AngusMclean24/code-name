826c08b2924ef7c816277bade077550bb042fa43
Angus Mclean <48145878+AngusMclean24@users.noreply.github.com>
2021-11-20 21:17:36 +1100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const socket = new WebSocket(serverAddress);
 // // Listen for messages
 socket.onmessage = ({ data }) => {
      console.log('Message from server ', data);
+     
+     for (i=0; i<data.length; i++){
+        if (data[i] == 0) {
+            document.getElementById(i+1).style.backgroundColor = "rgba(95, 91, 91, 0.8)";
+            //document.getElementById(i+1).textContent = 20
+
+        }
+        
+        else{
+            document.getElementById(i+1).style.backgroundColor = "rgba(192, 67, 67, 0.8)";
+            //document.getElementById(i+1).textContent = 10
+        }
+    } 
 };
 
 // document.querySelector('button').onclick = () => {
