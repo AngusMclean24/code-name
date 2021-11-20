7de802176075829eb0bc12010c54a633813ec4d4
Angus Mclean <48145878+AngusMclean24@users.noreply.github.com>
2021-11-20 17:51:36 +1100

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+
+const socket = io('ws://localhost:8080');
+
+socket.on('message', text => {
+    /*
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el)
+    */
+
+
+    for (i=0; i<text.length; i++){
+        if (text[i] == 0) {
+            document.getElementById(i+1).style.backgroundColor = "rgba(95, 91, 91, 0.8)";
+            //document.getElementById(i+1).textContent = 20
+
+        }
+        
+        else{
+            document.getElementById(i+1).style.backgroundColor = "rgba(192, 67, 67, 0.8)";
+            //document.getElementById(i+1).textContent = 10
+        }
+    } 
+});
+
+/*
+document.querySelector('button').onclick = () => {
+
+    const text = document.querySelector('input').value;
+    socket.emit('message', text)
+    
+}
+*/
+function colourChange(item){
+    socket.emit('message', item)
+
+}
