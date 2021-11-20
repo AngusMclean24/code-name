b4fe6b0b322f481dada02619c75c44cecce68e2f
Angus Mclean <48145878+AngusMclean24@users.noreply.github.com>
2021-11-20 21:31:39 +1100

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const serverAddress = 'wss://simple-websocket-codename.glitch.me/';
 const socket = new WebSocket(serverAddress); 
 // // Listen for messages
 socket.onmessage = ({ data }) => {
+     grid = JSON.parse(data)
+     console.log('Message from server ', grid);
      
-     console.log('Message from server ', JSON.parse(data));
-     
-     for (i=0; i<data.length; i++){
-        if (data[i] == 0) {
+     for (i=0; i<grid.length; i++){
+        if (grid[i] == 0) {
             document.getElementById(i+1).style.backgroundColor = "rgba(95, 91, 91, 0.8)";
             //document.getElementById(i+1).textContent = 20
 
